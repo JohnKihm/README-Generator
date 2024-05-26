a434da65078ee1b7a1644ab15b24852209a38fda
JohnKihm <john.a.kihm@gmail.com>
2024-05-26 09:17:55 -0500

    created questions array in index.js

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const questions = [
+    'What is the title of your project?',
+    'Provide a short description explaining the what, why, and how of your project.',
+    'What are the steps required to install your project?',
+    'Provide instructions and examples for use.',
+    'How can other developers contribute to your project?',
+    'Enter instructions for testing your project.',
+    'Which license would you like to use',
+    'What is your GitHub username?',
+    'What is your email address?'
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

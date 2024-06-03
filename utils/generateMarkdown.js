2529a6a4f19a1a118ff3b6202c4e5e8e2fdfca79
JohnKihm <john.a.kihm@gmail.com>
2024-06-02 19:26:21 -0500

    created renderLicenseBadge function in generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,28 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  } else {
+    return `https://img.shields.io/badge/${license.split(' ').join('_')}-8A2BE2`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  } else {}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  } else {}
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {

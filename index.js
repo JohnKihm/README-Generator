a6cc8494fdee31646ddeed3d605720b058bcbc54
JohnKihm <john.a.kihm@gmail.com>
2024-06-02 20:47:22 -0500

    created writeToFile function in index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // Create an array of questions for user input
 const questions = [
@@ -15,8 +16,11 @@ const questions = [
     'What is your email address?'
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Create a function to write README file
+function writeToFile(fileName, data) {
+    const markdown = generateMarkdown(data);
+    fs.writeFile(fileName, markdown, (err) => err ? console.error(err) : console.log('Success!'));
+}
 
 // Create a function to initialize app
 function init() {
